9b33a6c738d81e4b19bf0703f36e7f206a8f32ab
cbig3 <romanvillagran386@gmail.com>
2026-03-25 16:13:18 -0700

    added the razor views

---
+++ COMP003B.Assignment2/Views/Projects/Ideas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment2.Views.Projects
+{
+    public class IdeasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
