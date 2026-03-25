6e127381d04fdafff189d9dcf293102ae7386e0c
cbig3 <romanvillagran386@gmail.com>
2026-03-25 16:08:31 -0700

    added the controller file with the Current, completed, and ideas

--- COMP003B.Assignment2/Controllers/ProjectControllers.cs
+++ COMP003B.Assignment2/Controllers/ProjectControllers.cs
@@ -1,6 +1,22 @@
-﻿namespace COMP003B.Assignment2.Controllers
+﻿using Microsoft.AspNetCore.Mvc; 
+namespace COMP003B.Assignment2.Controllers
 {
-    public class ProjectControllers
+    public class ProjectControllers : Controller
     {
+        [HttpGet]
+        public IActionResult current()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Completed()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Ideas() {
+            return View();
+        }
     }
 }
